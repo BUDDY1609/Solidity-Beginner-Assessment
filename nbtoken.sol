a9e074c86dcafcf3a6889d9e5ebcce2509c39c7d
HIREN PATEL <111114081+BUDDY1609@users.noreply.github.com>
2023-06-24 13:55:46 +0530

    Update nbtoken.sol

--- nbtoken.sol
+++ nbtoken.sol
@@ -1,25 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
-
-contract nvstoken {
+contract nbtoken {
 
     // public variables here
     string public tokenName = "NOBUDDY";
-    string public tokenAbb = "NB";
+    string public tokenAbbrv = "NB";
     uint public totalSupply = 0;
 
     // mapping variable here
@@ -28,7 +14,7 @@ contract nvstoken {
  
     // mint function
 
-    function mint(address _address, uint _value) public {
+    function mintToken(address _address, uint _value) public {
 
         totalSupply += _value;
         balances[_address] += _value;
@@ -36,7 +22,7 @@ contract nvstoken {
 
     // burn function
 
-    function burn(address _address, uint _value) public{
+    function burnToken(address _address, uint _value) public{
 
         if (balances[_address] >= _value){
             totalSupply -= _value;
